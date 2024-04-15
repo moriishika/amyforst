6d6dfbb6361bbcb8a0a08789fe20e4fc8b8411f8
ramanavami32 <ramanavami32@gmail.com>
2024-04-15 11:13:10 +0000

    another changes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,6 @@ export default function Document() {
     <Html lang="en">
     <Head>
         <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <title>My awesome PWA app</title>
         <meta name="description" content="Best PWA app in the world!" />
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="manifest" href="/manifest.json" />
