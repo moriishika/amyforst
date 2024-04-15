33d44d7526db891589419c7bf831804c9b297d10
ramanavami32 <ramanavami32@gmail.com>
2024-04-15 11:10:15 +0000

    some changes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,9 +3,18 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
+    <Head>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <title>My awesome PWA app</title>
+        <meta name="description" content="Best PWA app in the world!" />
+        <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="manifest" href="/manifest.json" />
-    </Head>
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="My awesome PWA app" />
+        <meta property="og:description" content="Best PWA app in the world!" />
+        <meta property="og:site_name" content="Forst Eternity" />
+        <meta property="og:url" content="https://amyforst.vercel.app" />
+      </Head>
       <body>
         <Main />
         <NextScript />
