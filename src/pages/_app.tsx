f6df765b9d320440b962f217f6349dd56010c816
Morii Shikaa <48641207+moriishika@users.noreply.github.com>
2024-04-16 11:34:22 +1000

    I think it's good now

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <>
+    <Head>
+      <meta name="viewport" content="width=device-width,initial-scale=1" />
+      <title>Forst Eternity</title>
+      <meta name="description" content="Best PWA app in the world!" />
+      <link rel="shortcut icon" href="/favico.png" type="image/x-icon" />
+      <link rel="manifest" href="/manifest.json" />
+    </Head>
     <Component {...pageProps} />;
   </>
 }
